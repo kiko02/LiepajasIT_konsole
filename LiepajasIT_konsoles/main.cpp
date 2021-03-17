ed33a3a6ef0dfbb672d4f8384ddf5f4fa766d6e3
kiko02 <kristofers.barkans@gmail.com>
2021-03-18 01:09:02 +0200

    add all files

--- LiepajasIT_konsoles/main.cpp
+++ LiepajasIT_konsoles/main.cpp
@@ -1,9 +1,35 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <windows.h>
 
 using namespace std;
+void color(int i){
+HANDLE hCon;
 
+WORD  wAttributes;
+if(i==1){
+
+  wAttributes = BACKGROUND_RED | FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN;
+}else{
+    wAttributes =  FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN;
+}
+
+hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+
+SetConsoleTextAttribute(hCon, wAttributes);
+
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    int i=0;
+
+while(1){
+color(i);
+if(i)
+cout<<"TEST";
+else
+cout<<"test";
+i=!i;
+
+}
     return 0;
 }
